da92a1a61566d6aebc386fce4d78ae68d93b57d8
Harun <mdharunbd88@gmail.com>
2020-10-08 13:30:40 +0600

    added another chart

--- js/main.js
+++ js/main.js
@@ -30,4 +30,56 @@ $(document).ready(function(){
             }
         }
     });
+
+    
+    var ctx_barChart = document.getElementById('barChart');
+    var barChart = new Chart(ctx_barChart, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: 'Legend 1',
+                data: [15, 30, 13, 15, 20, 30],
+                backgroundColor: [
+                    '#96C0FF',
+                    '#0066FF;',
+                    '#96C0FF',
+                    '#0066FF;',
+                    '#96C0FF',
+                    '#0066FF;',
+                ]
+            }
+        ]
+        },
+        options: {
+            legend:{
+                display:false
+            },
+            scales: {
+              yAxes: [{
+                gridLines: {
+                  drawBorder: false,
+                  drawOnChartArea:false,
+                  color: "rgba(0, 0, 0, 0)",
+                },
+                ticks: {
+                    display: false,
+                  },
+              }],
+              xAxes: [{
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                    drawBorder:false,
+                    drawOnChartArea:false
+                },
+                ticks: {
+                    display: false,
+                }
+            }],
+              
+            },
+          },
+    });
+    
+    
 });
